cd6590908ad737befd053fa4c70a8f41716d928a
Mariana <marianadejesussilva921@gmail.com>
2024-10-30 03:21:32 -0300

    ADD: Functions Date Now and Filters.

--- todo/src/service/todo.service.ts
+++ todo/src/service/todo.service.ts
@@ -1,9 +1,46 @@
+import { Time } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+export interface Task {
+  id: number;
+  title: string;
+  description: string;
+  date: Date;
+  time: Time;
+  status: boolean;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  private api = 'http://localhost:3000/taks';
+  private tasks : Task[] = [];
+  private nextId = 1
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getTasks(): Observable <Task[]> {
+    return this.http.get<Task[]>(this.api);
+  }
+  addTask(task: Task): Observable <Task>{
+    task.id = this.nextId++;
+    this.tasks.push(task);
+    return this.http.post<Task>(this.api, task);
+  }
+
+  updateTask(task: Task): Observable<Task> {
+    const index = this.tasks.findIndex(i => i.id === task.id);
+    if (index !== -1){
+      this.tasks[index] = task;
+    }
+    return  this.http.put<Task>(`${this.api}/${task.id}`, task);
+  }
+
+
+  deleteTask(id: number): Observable<void> {
+    this.tasks = this.tasks.filter(task => task.id !== id);
+    return this.http.delete<void>(`${this.api}/${id}`)
+  }
 }
--- todo/src/todo/todo.component.ts
+++ todo/src/todo/todo.component.ts
@@ -1,14 +1,95 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Task, TodoService } from '../service/todo.service';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    HttpClientModule],
+  providers: [TodoService],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css'
 })
 export class TodoComponent {
+  tasks: Task[] = [];
+  nowdate: Date | undefined;
+  editTask: Task | null = null;
+  filteredTasks: Task[] = [];
+  completedTasks: Task[] = [];
+  newTask: Task = {
+    id: 0,
+    title: '',
+    description: '',
+    date: new Date(),
+    time: {
+      hours: 0,
+      minutes: 0
+    },
+    status: false
+  }
+
+  constructor(private todoService: TodoService) {
+
+  }
+
+  ngOnInit() {
+    this.nowdate = new Date();
+    this.loadTasks();
+  }
+
+  loadTasks() {
+    this.todoService.getTasks().subscribe(task => {
+      this.tasks = task;
+      this.filteredTasks = this.tasks.filter(task => task.status === false);
+      this.completedTasks = this.tasks.filter(task => task.status === true);
+
+    });
+  }
+  toggleStatus(task: Task) {
+    task.status = !task.status;
+    this.todoService.updateTask(task).subscribe();
+    this.loadTasks();
+  }
+
+  editItem(task: Task) {
+    this.editTask = { ...task };
+  }
+
+  addTask() {
+    this.todoService.addTask(this.newTask).subscribe(task => {
+      this.tasks.push(task);
+      this.newTask = {
+        id: 0,
+        title: '',
+        description: '',
+        date: new Date(),
+        time: {
+          hours: 0,
+          minutes: 0
+        },
+        status: false
+      }
+    })
+  }
+  updateTask() {
+    if (this.editTask) {
+      this.todoService.updateTask(this.editTask).subscribe(task => {
+        const index = this.tasks.findIndex(i => i.id === task.id);
+        if (index !== -1) this.tasks[index] = task;
+        this.editTask = null;
+      });
+    }
+  }
+  deleteItem(id: number) {
+    this.todoService.deleteTask(id).subscribe(() => {
+      this.tasks = this.tasks.filter(task => task.id !== id); // Remove o item da lista
+    });
+  }
 
 }
+
